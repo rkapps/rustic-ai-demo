05b604c3e49dc9c325a725045cc09558973fdbee
Raghu Kasavajhula <rkapps.gae@gmail.com>
2026-05-12 20:40:10 -0700

    env

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    chatUrl: 'http://localhost:8080',
+    firebase: {
+        apiKey: "REPLACE_ME",
+        authDomain: "REPLACE_ME",
+        projectId: "REPLACE_ME",
+        storageBucket: "REPLACE_ME",
+        messagingSenderId: "REPLACE_ME",
+        appId: "REPLACE_ME"
+      }
+};
